d3e3fa5c7b90578850d4e03aaaa40c69df8be671
mbabi <mbabiak39@gmail.com>
2024-06-30 12:09:40 +0200

    change source to zip

--- terraform/cloudfunction.tf
+++ terraform/cloudfunction.tf
@@ -4,7 +4,7 @@ resource "google_cloudfunctions_function" "function" {
   available_memory_mb = 256
   runtime = "python39"
   source_archive_bucket = "cloud_functions_dev"
-  source_archive_object = "cloudfunction/main.py"
+  source_archive_object = "pluto.zip"
   entry_point = "pubsub_to_bigquery"
   timeout = 60
   event_trigger {
