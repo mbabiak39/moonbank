4e2680ba83591199f9168365a2b52003765cd8e6
mbabi <mbabiak39@gmail.com>
2024-06-30 14:28:45 +0200

    add monitoring roles

--- terraform/pub_sub.tf
+++ terraform/pub_sub.tf
@@ -10,7 +10,7 @@ resource "google_pubsub_topic" "idle_vm_topic" {
   message_retention_duration = "10000s"
 }
 
-resource "google_pubsub_subscription" "idle_vm_subscription" {
+resource "google_pubsub_subscription" "subscription" {
   project = var.project_id
   name = "${var.project_id}-catchall"
   topic = google_pubsub_topic.topic.name
