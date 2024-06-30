60eccea4b024bbf4313eddd94cb99d47a414e52c
mbabi <mbabiak39@gmail.com>
2024-06-30 11:40:39 +0200

    change cloudfunction to source code from bucket

--- terraform/cloudfunction.tf
+++ terraform/cloudfunction.tf
@@ -1,15 +1,14 @@
 resource "google_cloudfunctions_function" "function" {
-  project             = var.project_id
-  name                = var.function
+  project = var.project_id
+  name = var.function
   available_memory_mb = 256
-  runtime             = "python39"
-  source_repository {
-    url = "https://github.com/mbabiak39/PLUTO/blob/master/cloudfunction/main.py"
-  }
-  entry_point       = "pubsub_to_bigquery"
+  runtime = "python39"
+  source_archive_bucket = "gs://cloud_functions_dev"
+  source_archive_object = "cloudfunction"
+  entry_point = "pubsub_to_bigquery"
   timeout = 60
   event_trigger {
-    resource   = google_pubsub_topic.topic.name
+    resource = google_pubsub_topic.topic.name
     event_type = "google.pubsub.topic.publish"
   }
 }
