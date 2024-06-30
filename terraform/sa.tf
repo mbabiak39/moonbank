1b277e6400967b1e4da0842fde186df262f6a459
mbabi <mbabiak39@gmail.com>
2024-06-30 14:27:04 +0200

    add monitoring roles

--- terraform/sa.tf
+++ terraform/sa.tf
@@ -1,19 +1,19 @@
 resource "google_service_account" "service_account" {
-  account_id   = var.function_sa
+  account_id = var.function_sa
 }
 
 resource "google_project_iam_member" "function_sa_binding" {
-  project       = var.project_id
-  role          = "roles/viewer"
-  member        = "serviceAccount:${google_service_account.service_account.email}"
+  project = var.project_id
+  role = "roles/viewer"
+  member = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_service_account" "cloud_build" {
-  account_id   = "cloud-build"
+  account_id = "cloud-build"
   display_name = "Service account for Cloud build deployments"
 }
 
-resource "google_project_iam_member" "service_account_roles" {
+resource "google_project_iam_member" "cloud_build_roles" {
   for_each = toset([
     "roles/cloudasset.owner",
     "roles/cloudbuild.builds.builder",
@@ -25,6 +25,15 @@ resource "google_project_iam_member" "service_account_roles" {
     "roles/viewer"
   ])
   project = var.project_id
-  member  = "serviceAccount:${google_service_account.cloud_build.email}"
-  role    = each.key
+  member = "serviceAccount:${google_service_account.cloud_build.email}"
+  role = each.key
+}
+
+resource "google_project_iam_member" "monitoring_roles" {
+  for_each = toset([
+    "roles/pubsub.publisher"
+  ])
+  project = var.project_id
+  member = "serviceAccount: service-${data.google_project.project.number}@gcp-sa-monitoring-notification.iam.gserviceaccount.com"
+  role = each.key
 }
